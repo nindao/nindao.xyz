4da165383ba9723b6bbb6e548b3050b1fbf9aea2
the-sociophobic <torikowashi@gmail.com>
2022-04-08 08:42:55 +0300

    add Sections

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -7,9 +7,12 @@ import SectionWithBackground from './SectionWithBackground'
 import Background from './Background'
 
 import backgroundImg from '../styles/img/background.jpg'
+import Section from './Section'
 
 const Site: React.FC = () => {
   const store = React.useContext(Context)
+  const sections = store?.contentful?.pages?.[0]?.sections
+    ?.filter((section: any) => section.type === 'section')
 
   return store.ready ?
     <div className="App">
@@ -19,6 +22,15 @@ const Site: React.FC = () => {
       >
         <Background />
       </SectionWithBackground>
+      {sections.map((section: any, index: number) =>
+        <Section
+          title={section.title}
+          bullets={section.bullets}
+          image={section.image}
+          imageMobile={section.imageMobile}
+          imageToRight={!!(index % 2)}
+        />
+      )}
       <Footer />
     </div>
     :
