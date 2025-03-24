96578bbb0c217f1505af9473473f096f18996d68
the-sociophobic <torikowashi@gmail.com>
2025-03-24 20:34:38 +0300

    add digistore24-scripts to nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 import type { Metadata } from 'next'
 
 import '@/app/lib/styles/index.sass'
@@ -14,6 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <Script src="https://www.digistore24-scripts.com/service/digistore.js"></Script>
+        <Script type="text/javascript">
+        {`
+          digistorePromocode( {"product_id": 602817, "adjust_domain": true } );
+        `}
+        </Script>
+      </head>
       <body>
         {children}
       </body>
